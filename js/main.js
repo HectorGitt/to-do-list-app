cb1aba35efd9b96baf1fc22b624024da65265d8f
Adeniyi Olaitan <adeniyi.olaitanhector@yahoo.com>
2021-01-30 00:53:57 +0100

    Update main.js
    added caching functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var listItems = document.getElementById("list");
 var add = document.getElementById("add");
 var typed = document.getElementsByTagName("input");
 var removebut = document.getElementById("remove");
+var count = Object.keys(localStorage).length
 //Event
 function append(){
     var todo = document.createElement('li')
@@ -9,6 +10,9 @@ function append(){
         todo.innerHTML = '<h3>'+ typed[0].value +'</h3><button id="remove" type="button">X</button>';
         listItems.appendChild(todo);
         typed[0].value = '';
+        todoItem = list.innerHTML;
+        localStorage.setItem("todo", JSON.stringify(todoItem));
+
     }
 
 }
@@ -19,9 +23,17 @@ function clear(e){
     if (e.target.id === 'remove'){
         if (confirm("Are you Sure")){
             var todo = e.target.parentElement;
+            todoItem = e.target.parentElement.getElementsByTagName("h3")[0].innerText
             listItems.removeChild(todo);
+            todoItem = list.innerHTML;
+            localStorage.setItem("todo", JSON.stringify(todoItem));
 
         }
     }
 }
 addEventListener("click", clear);
+function getItems() {
+    var items = JSON.parse(localStorage.getItem("todo"));
+    listItems.innerHTML = items;
+}
+getItems();
